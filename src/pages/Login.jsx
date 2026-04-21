3f3d49d253b47693f85e9a85db5865901a02e939
Luan martins <luanmartinsdecarvalho000@gmail.com>
2026-04-21 20:53:52 -0300

    Comunidade

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -77,7 +77,7 @@ export default function Login() {
       {/* Card */}
       <form
         onSubmit={handleLogin}
-        className="relative z-10 w-[380px] p-8 rounded-2xl 
+        className="relative z-10 w-95 p-8 rounded-2xl 
         bg-white/10 backdrop-blur-xl border border-white/30 shadow-2xl
         animate-fade-in"
       >
